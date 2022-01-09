9194977f1e3461f2db1bc4758a4cb75a87668e23
MarcioXara <marcioxara@gmail.com>
2022-01-08 23:12:30 -0300

    primeiro commit

---
+++ 003.py
@@ -0,0 +1,23 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('digite outro numero: '))
+s = n1 + n2
+print('\033[4;33;33m A soma entre {} e {} é {}! \033[m'.format(n1, n2, s))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
